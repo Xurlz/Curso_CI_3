7c846a274e799f90a8d407751f35708a18dac796
Charles Sena <charlessena@gokeitecnologia.com.br>
2025-11-13 10:16:21 -0300

    ci: Updates for personal use

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"log"
 	"os"
+	"fmt"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -15,7 +16,15 @@ var (
 
 func ConectaComBancoDeDados() {
 	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
+	stringDeConexao := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("HOST"),
+		os.Getenv("USER"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("DBNAME"),
+		os.Getenv("PORT"),
+	)
+
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
